daf5cdeba635885a8fb21fd26f1453f06d1bcd80
Sourabh-developer18 <sourabh.developer18@gmail.com>
2025-11-19 19:48:40 +0530

    fixed proxy in google strategy

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -40,7 +40,8 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    proxy: true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
